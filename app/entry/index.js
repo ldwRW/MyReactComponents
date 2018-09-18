3b53bd398b3787fb0c70d7aabe58e66bb30c9349
ldw <2465970692@qq.com>
2018-09-18 10:59:01 +0800

    fix bugs

--- app/entry/index.js
+++ app/entry/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from 'routes/routes'
-import 'app/entry/index.css'
+import 'app/entry/index.scss'
 
 ReactDOM.render(
   <Routes />, 
