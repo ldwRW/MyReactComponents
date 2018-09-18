eac3a2b2d924f19e91790f539d3df7dcff95be15
ldw <2465970692@qq.com>
2018-09-18 10:36:00 +0800

    format this file.

--- app/entry/index.js
+++ app/entry/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom'
 import Routes from 'routes/routes'
 import 'app/entry/index.css'
 
-ReactDOM.render(<Routes />, document.getElementById('root'))
+ReactDOM.render(
+  <Routes />, 
+  document.getElementById('root')
+)
