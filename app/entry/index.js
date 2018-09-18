c4208e65ff63c98c86e961025c178e55b4274e00
ldw <2465970692@qq.com>
2018-09-18 10:03:29 +0800

    Basic configuration, and dependencies.

---
+++ app/entry/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from 'routes/routes'
+import 'app/entry/index.css'
+
+ReactDOM.render(<Routes />, document.getElementById('root'))
---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        app: './app/entry/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                //use: ['babel-loader?presets[]=react']
+                use: [
+                    { loader: "babel-loader" }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                use: [
+                    { loader: "sass-loader"}
+                ]
+            }
+        ]
+    },
+    resolve: {
+        modules: ['node_modules'],
+        alias: {
+            conponents: path.resolve(__dirname, 'app/components'),
+            routes: path.resolve(__dirname, 'app/routes'),
+            app: path.resolve(__dirname, 'app'),
+        }
+    }
+}
